2508dadb33b4668accfc25892ffe0e687c968d2a
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-08-30 18:57:20 +0400

    Adjusting attribute to be set on script

--- inject.js
+++ inject.js
@@ -15,8 +15,7 @@ export default ({ router }) => {
       g=d.createElement('script'),
       s=d.getElementsByTagName('script')[0];
 
-      g.type='text/javascript';
-      g.setAttribute('website-id', SITE_ID);
+      g.setAttribute('data-website-id', SITE_ID);
       g.async=true;
       g.defer=true;
       g.src=TRACKER_URL+'/umami.js';
