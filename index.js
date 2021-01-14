13c878bb3d8249135348f3d489c78b094ff1fd1e
EloEntropy <alecwvr0@gmail.com>
2021-01-14 15:28:48 +0800

    Add optional umami configurations (#2)
    * added option for respectingDo Not Track request from browser
    
    * changed default to false
    
    * added additional config options and updated spacing
    
    Co-authored-by: aweaver <aweaver@vautomation.tech>

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options = {}, context) => ({
   define () {
+    // REQUIRED CONFIG
     const { siteConfig = {} } = context
     const trackerUrl = options.trackerUrl || siteConfig.trackerUrl
     const siteId = options.siteId || siteConfig.siteId
     const TRACKER_URL = trackerUrl || false
     const SITE_ID = siteId || false
 
-    return { TRACKER_URL, SITE_ID }
+    // OPTIONAL CONFIG
+    const DATA_HOST_URL = options.dataHostURL != null ? options.dataHostURL : "" // Allow sending tracking data to URL separate from host umami.js URL
+    const DATA_AUTO_TRACK = options.dataAutoTrack != null ? options.dataAutoTrack : true // Configure umami's default auto tracking
+    const DO_NOT_TRACK = options.doNotTrack != null ? options.doNotTrack : false // Option to respect user Do Not Track requests, default to false to preserve existing functionality
+    const DATA_CACHE = options.dataCache != null ? options.dataCache : false // Allow data caching, using localhost storage
+    const DATA_DOMAINS = options.dataDomains != null ? options.dataDomains : "" // Limit tracking to occur on specific domains
+
+    return { TRACKER_URL, SITE_ID, DATA_HOST_URL, DATA_AUTO_TRACK, DO_NOT_TRACK, DATA_CACHE, DATA_DOMAINS }
   },
 
   enhanceAppFiles: [
